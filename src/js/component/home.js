58b9a2fee563451c0a71df0e384064d61b728ca3
Ivette Dominguez <ivettedominguez104@gmail.com>
2020-01-03 02:43:38 +0000

    functional add list item and some style

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [newTodo, setNewTodo] = useState("");
+	const [toDos, setToDos] = useState([]);
+
+	function handleNewTodo(e) {
+		e.preventDefault();
+		setNewTodo(e.target.value);
+	}
+
+	function handleToDos(e) {
+		e.preventDefault();
+		if (newTodo === "") return;
+		setToDos([...toDos, { id: Date.now(), text: newTodo }]);
+		e.target.reset();
+
+		function removeTodo(id) {
+			setToDos(toDos.filter(todo => todo.id != id));
+		}
+		console.log(newTodo);
+	}
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="head mx-auto">
+				<h1>ToDo List</h1>
+			</div>
+			<form onSubmit={handleToDos}>
+				<input
+					className="input"
+					placeholder="new to do"
+					onChange={handleNewTodo}
+				/>
+				<ul className="todos mx-auto">
+					{toDos.map(todo => (
+						<li key={todo.id}>
+							{" "}
+							{todo.text}
+							<a
+								href="#"
+								className="remove float-right"
+								onClick={() => removeTodo(todo.id)}>
+								{" "}
+								<i class="fas fa-times" />
+							</a>
+						</li>
+					))}
+				</ul>
+			</form>
 		</div>
 	);
 }
